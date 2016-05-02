57fc19f09e8541bbaa4e40bd8521670780b8656a
Terry Howe <terrylhowe@gmail.com>
2016-05-02 15:09:16 -0600

    Version 0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from distutils.core import setup
 
 files = [
-    "ansible/module_utils/hashivault",
+    "ansible/module_utils",
     "ansible/modules/hashivault",
 ]
 
 setup(name='ansible-modules-hashivault',
-      version='0.3',
+      version='0.4',
       description='Ansible Modules for Hashicorp Vault',
       author='Terry Howe',
       author_email='terrylhowe@example.com',
