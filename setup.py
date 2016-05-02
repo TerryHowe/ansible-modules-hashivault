df686238131140e1448bc6da3fce417d386eca1c
Terry Howe <terrylhowe@gmail.com>
2016-05-02 15:53:30 -0600

    Clean up init file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ files = [
 ]
 
 setup(name='ansible-modules-hashivault',
-      version='0.5',
+      version='0.6',
       description='Ansible Modules for Hashicorp Vault',
       author='Terry Howe',
       author_email='terrylhowe@example.com',
