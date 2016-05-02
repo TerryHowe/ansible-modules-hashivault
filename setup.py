d9339a48ecd12b5f41b2f4a883afe9c6938baa5d
Terry Howe <terrylhowe@gmail.com>
2016-05-02 15:56:02 -0600

    bump version because of bad upload to pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ files = [
 ]
 
 setup(name='ansible-modules-hashivault',
-      version='0.6',
+      version='0.7',
       description='Ansible Modules for Hashicorp Vault',
       author='Terry Howe',
       author_email='terrylhowe@example.com',
