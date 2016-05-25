fdd0290116819739d485469fda6f1f9b0d13a3e8
Terry Howe <terrylhowe@gmail.com>
2016-05-25 11:33:17 -0600

    Add long description

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ files = [
 ]
 
 setup(name='ansible-modules-hashivault',
-    version='1.1.2',
+    version='1.1.3',
     description='Ansible Modules for Hashicorp Vault',
+    long_description=open("README.rst").read(),
     author='Terry Howe',
     author_email='terrylhowe@example.com',
     url='https://github.com/TerryHowe/ansible-modules-hashivault',
