d36b6795ea9f0354231cd20613cb40ff06ee757a
Terry Howe <terrylhowe@gmail.com>
2016-10-08 09:37:35 -0600

    add policy get

---
+++ ansible/modules/hashivault/hashivault_policy_get.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+DOCUMENTATION = '''
+---
+module: hashivault_policy_get
+version_added: "1.2.0"
+short_description: Hashicorp Vault policy get module
+description:
+    - Module to get a policy from Hashicorp Vault.
+options:
+    url:
+        description:
+            - url for vault
+        default: to environment variable VAULT_ADDR
+    verify:
+        description:
+            - verify TLS certificate
+        default: to environment variable VAULT_SKIP_VERIFY
+    authtype:
+        description:
+            - authentication type to use: token, userpass, github, ldap
+        default: token
+    token:
+        description:
+            - token for vault
+        default: to environment variable VAULT_TOKEN
+    username:
+        description:
+            - username to login to vault.
+        default: False
+    password:
+        description:
+            - password to login to vault.
+        default: False
+    name:
+        description:
+            - policy name.
+        default: False
+'''
+EXAMPLES = '''
+---
+- hosts: localhost
+  tasks:
+    - hashivault_policy_get:
+      name: 'annie'
+      register: 'vault_policy_get'
+    - debug: msg="User policy is {{vault_policy_get.policy}}"
+'''
+
+
+def main():
+    argspec = hashivault_argspec()
+    argspec['name'] = dict(required=True, type='str')
+    module = hashivault_init(argspec)
+    result = hashivault_policy_get(module.params)
+    if result.get('failed'):
+        module.fail_json(**result)
+    else:
+        module.exit_json(**result)
+
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.hashivault import *
+
+if __name__ == '__main__':
+    main()
