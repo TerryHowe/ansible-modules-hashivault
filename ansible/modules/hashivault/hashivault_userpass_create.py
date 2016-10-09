5fab2670109d856847c93f8bca71e04d9d213ca0
Terry Howe <terrylhowe@gmail.com>
2016-10-09 08:53:12 -0600

    Add userpass create and delete

---
+++ ansible/modules/hashivault/hashivault_userpass_create.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+DOCUMENTATION = '''
+---
+module: hashivault_userpass_create
+version_added: "2.2.0"
+short_description: Hashicorp Vault userpass create module
+description:
+    - Module to create userpass users in Hashicorp Vault.
+options:
+    url:
+        description:
+            - url for vault
+        default: to environment variable VAULT_ADDR
+    verify:
+        description:
+            - verify TLS certificate
+        default: to environment variable VAULT_SKIP_VERIFY
+    authtype:
+        description:
+            - authentication type to use: token, userpass, github, ldap
+        default: token
+    token:
+        description:
+            - token for vault
+        default: to environment variable VAULT_TOKEN
+    username:
+        description:
+            - username to login to vault.
+        default: False
+    password:
+        description:
+            - password to login to vault.
+        default: False
+    name:
+        description:
+            - user name to create.
+        default: False
+    pass:
+        description:
+            - user to create password.
+        default: False
+    policies:
+        description:
+            - user policies.
+        default: default
+'''
+EXAMPLES = '''
+---
+- hosts: localhost
+  tasks:
+    - hashivault_userpass_create:
+      name: 'bob'
+      pass: 'S3cre7s'
+      policies: 'bob'
+'''
+
+
+def main():
+    argspec = hashivault_argspec()
+    argspec['name'] = dict(required=True, type='str')
+    argspec['pass'] = dict(required=True, type='str')
+    argspec['policies'] = dict(required=False, type='str', default='default')
+    module = hashivault_init(argspec)
+    result = hashivault_userpass_create(module.params)
+    if result.get('failed'):
+        module.fail_json(**result)
+    else:
+        module.exit_json(**result)
+
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.hashivault import *
+
+
+@hashiwrapper
+def hashivault_userpass_create(params):
+   client = hashivault_client(params)
+   name = params.get('name')
+   password = params.get('pass')
+   policies = params.get('policies')
+   client.create_userpass(name, password, policies)
+   return {'changed': True}
+
+
+if __name__ == '__main__':
+    main()
---
+++ ansible/modules/hashivault/hashivault_userpass_delete.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+DOCUMENTATION = '''
+---
+module: hashivault_userpass_delete
+version_added: "2.2.0"
+short_description: Hashicorp Vault userpass delete module
+description:
+    - Module to delete userpass users in Hashicorp Vault.
+options:
+    url:
+        description:
+            - url for vault
+        default: to environment variable VAULT_ADDR
+    verify:
+        description:
+            - verify TLS certificate
+        default: to environment variable VAULT_SKIP_VERIFY
+    authtype:
+        description:
+            - authentication type to use: token, userpass, github, ldap
+        default: token
+    token:
+        description:
+            - token for vault
+        default: to environment variable VAULT_TOKEN
+    username:
+        description:
+            - username to login to vault.
+        default: False
+    password:
+        description:
+            - password to login to vault.
+        default: False
+    name:
+        description:
+            - user name.
+        default: False
+'''
+EXAMPLES = '''
+---
+- hosts: localhost
+  tasks:
+    - hashivault_userpass_delete:
+      name: 'bob'
+'''
+
+
+def main():
+    argspec = hashivault_argspec()
+    argspec['name'] = dict(required=True, type='str')
+    module = hashivault_init(argspec)
+    result = hashivault_userpass_delete(module.params)
+    if result.get('failed'):
+        module.fail_json(**result)
+    else:
+        module.exit_json(**result)
+
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.hashivault import *
+
+
+@hashiwrapper
+def hashivault_userpass_delete(params):
+   client = hashivault_client(params)
+   username = params.get('name')
+   client.delete_userpass(username)
+   return {'changed': True}
+
+
+if __name__ == '__main__':
+    main()
