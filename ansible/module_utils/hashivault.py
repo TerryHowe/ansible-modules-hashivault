b518d68fa41850a7a448e6cc10a239f026a4fc79
Terry Howe <terrylhowe@gmail.com>
2016-10-08 19:21:05 -0600

    Use a method decorator

--- ansible/module_utils/hashivault.py
+++ ansible/module_utils/hashivault.py
@@ -43,154 +43,106 @@ def hashivault_client(params):
     return client
 
 
-def hashivault_status(params):
-    result = { "changed": False, "rc" : 0}
-    try:
-        client = hashivault_client(params)
-        result['status'] = client.seal_status
-    except Exception as e:
-        import traceback
-        result['rc'] = 1
-        result['failed'] = True
-        result['msg'] = "Exception: " + str(e)
-        result['stack_trace'] = traceback.format_exc()
-    return result
-
-
-def hashivault_seal(params):
-    result = { "changed": False, "rc" : 0}
-    try:
-        key = params.get('key')
-        client = hashivault_client(params)
-        result['status'] = client.seal()
-        result['changed'] = True
-    except Exception as e:
-        import traceback
-        result['rc'] = 1
-        result['failed'] = True
-        result['msg'] = "Exception: " + str(e)
-        result['stack_trace'] = traceback.format_exc()
-    return result
-
-
-def hashivault_unseal(params):
-    result = { "changed": False, "rc" : 0}
-    try:
-        keys = params.get('keys')
-        client = hashivault_client(params)
-        result['status'] = client.unseal_multi(keys.split())
-        result['changed'] = True
-    except Exception as e:
-        import traceback
-        result['rc'] = 1
-        result['failed'] = True
-        result['msg'] = "Exception: " + str(e)
-        result['stack_trace'] = traceback.format_exc()
-    return result
-
-
-def hashivault_policy_get(params):
-    result = { "changed": False, "rc" : 0}
-    try:
-        client = hashivault_client(params)
-        name = params.get('name')
-        result['rules'] = client.get_policy(name)
-    except Exception as e:
-        import traceback
-        result['rc'] = 1
-        result['failed'] = True
-        result['msg'] = "Exception: " + str(e)
-        result['stack_trace'] = traceback.format_exc()
-    return result
-
-
-def hashivault_policy_list(params):
-    result = { "changed": False, "rc" : 0}
-    try:
-        client = hashivault_client(params)
-        result['policies'] = client.list_policies()
-    except Exception as e:
-        import traceback
-        result['rc'] = 1
-        result['failed'] = True
-        result['msg'] = "Exception: " + str(e)
-        result['stack_trace'] = traceback.format_exc()
-    return result
-
-
-def hashivault_policy_set(params):
-    result = { "changed": False, "rc" : 0}
-    try:
-        client = hashivault_client(params)
-        name = params.get('name')
-        rules = params.get('rules')
-        client.set_policy(name, rules)
-        result['changed'] = True
-    except Exception as e:
-        import traceback
-        result['rc'] = 1
-        result['failed'] = True
-        result['msg'] = "Exception: " + str(e)
-        result['stack_trace'] = traceback.format_exc()
-    return result
-
-
-def hashivault_read(params):
-    result = { "changed": False, "rc" : 0}
-    try:
-        client = hashivault_client(params)
-        secret = params.get('secret')
-        key = params.get('key')
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            response = client.read('secret/%s' % secret)
-            if not response:
-                result['rc'] = 1
-                result['failed'] = True
-                result['msg'] = "Secret %s is not in vault" % secret
-                return result
-            data = response['data']
-        if key not in data:
+def hashiwrapper(function):
+    def wrapper(*args, **kwargs):
+        result = { "changed": False, "rc" : 0}
+        try:
+            result.update(function(*args, **kwargs))
+        except Exception as e:
+            import traceback
             result['rc'] = 1
             result['failed'] = True
-            result['msg'] = "Key %s is not in secret %s" % (key, secret)
+            result['msg'] = "Exception: " + str(e)
+            result['stack_trace'] = traceback.format_exc()
+        return result
+    return wrapper
+
+
+@hashiwrapper
+def hashivault_status(params):
+    client = hashivault_client(params)
+    return {'status': client.seal_status}
+
+
+@hashiwrapper
+def hashivault_seal(params):
+    key = params.get('key')
+    client = hashivault_client(params)
+    return {'status': client.seal(), 'changed': True}
+
+
+@hashiwrapper
+def hashivault_unseal(params):
+    keys = params.get('keys')
+    client = hashivault_client(params)
+    return {'status': client.unseal_multi(keys.split()), 'changed': True}
+
+@hashiwrapper
+def hashivault_policy_get(params):
+    name = params.get('name')
+    client = hashivault_client(params)
+    return {'rules': client.get_policy(name)}
+
+
+@hashiwrapper
+def hashivault_policy_list(params):
+    client = hashivault_client(params)
+    return {'policies': client.list_policies()}
+
+
+@hashiwrapper
+def hashivault_policy_set(params):
+   client = hashivault_client(params)
+   name = params.get('name')
+   rules = params.get('rules')
+   client.set_policy(name, rules)
+   return {'changed': True}
+
+
+@hashiwrapper
+def hashivault_read(params):
+    result = { "changed": False, "rc" : 0}
+    client = hashivault_client(params)
+    secret = params.get('secret')
+    key = params.get('key')
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        response = client.read('secret/%s' % secret)
+        if not response:
+            result['rc'] = 1
+            result['failed'] = True
+            result['msg'] = "Secret %s is not in vault" % secret
             return result
-        value = data[key]
-        result['value'] = value
-    except Exception as e:
-        import traceback
+        data = response['data']
+    if key not in data:
         result['rc'] = 1
         result['failed'] = True
-        result['msg'] = "Exception: " + str(e)
-        result['stack_trace'] = traceback.format_exc()
+        result['msg'] = "Key %s is not in secret %s" % (key, secret)
+        return result
+    value = data[key]
+    result['value'] = value
     return result
 
 
+@hashiwrapper
 def hashivault_write(params):
     result = { "changed": False, "rc" : 0}
-    try:
-        client = hashivault_client(params)
-        secret = params.get('secret')
-        data = params.get('data')
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            if params.get('update'):
-                read_data = client.read('secret/%s' % secret)
-                if read_data and 'data' in read_data:
-                    write_data  = read_data['data']
-                else:
-                    write_data  = {}
-                write_data.update(data)
-                client.write(('secret/%s' % secret), **write_data)
-                result['msg'] = "Secret %s updated" % secret
+    client = hashivault_client(params)
+    secret = params.get('secret')
+    data = params.get('data')
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        if params.get('update'):
+            read_data = client.read('secret/%s' % secret)
+            if read_data and 'data' in read_data:
+                write_data  = read_data['data']
             else:
-                client.write(('secret/%s' % secret), **data)
-                result['msg'] = "Secret %s written" % secret
-        result['changed'] = True
-    except Exception as e:
-        import traceback
-        result['rc'] = 1
-        result['failed'] = True
-        result['msg'] = "Exception: " + str(e)
-        result['stack_trace'] = traceback.format_exc()
+                write_data  = {}
+            write_data.update(data)
+            client.write(('secret/%s' % secret), **write_data)
+            result['msg'] = "Secret %s updated" % secret
+        else:
+            client.write(('secret/%s' % secret), **data)
+            result['msg'] = "Secret %s written" % secret
+    result['changed'] = True
     return result
