ee8b05b3250f86e0a311e2f798cadbaea6e20ce4
Terry Howe <terrylhowe@gmail.com>
2016-10-15 09:04:06 -0600

    update setup.py with readme

--- setup.py
+++ setup.py
@@ -6,38 +6,10 @@ files = [
     "ansible/modules/hashivault",
 ]
 
-long_description = """
-
-Ansible modules for Hashicorp Vault.
-
-Usage
------
-
-The following example writes the giant secret with two values and then
-reads the fie value::
-
-    ---
-    - hosts: localhost
-      tasks:
-        - hashivault_write:
-            secret: giant
-            data:
-                foo: foe
-                fie: fum
-        - hashivault_read:
-            secret: 'giant'
-            key: 'fie'
-          register: 'fie'
-        - debug: msg="Value is {{fie.value}}"
-
-If you are not using the VAULT_ADDR and VAULT_TOKEN environment variables,
-you may be able to simplify your playbooks with an action plugin.  This can
-be some somewhat similar to this `example action plugin <https://terryhowe.wordpress.com/2016/05/02/setting-ansible-module-defaults-using-action-plugins/>`_.
-
-"""
+long_description=open('README.rst', 'r').read()
 
 setup(name='ansible-modules-hashivault',
-    version='2.5.3',
+    version='2.5.4',
     description='Ansible Modules for Hashicorp Vault',
     long_description=long_description,
     author='Terry Howe',
