cfd6c3b7eee71e6d81ba68f17855a94ac7e41606
Terry Howe <terrylhowe@gmail.com>
2016-10-18 13:36:30 -0600

    use setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 files = [
     "ansible/module_utils",
@@ -8,16 +8,17 @@ files = [
 
 long_description=open('README.rst', 'r').read()
 
-setup(name='ansible-modules-hashivault',
-    version='2.5.5',
+setup(
+    name='ansible-modules-hashivault',
+    version='2.5.6',
     description='Ansible Modules for Hashicorp Vault',
     long_description=long_description,
     author='Terry Howe',
     author_email='terrylhowe@example.com',
     url='https://github.com/TerryHowe/ansible-modules-hashivault',
     packages=files,
-    requires = [
+    install_requires = [
+        'ansible>=2.0.0',
         'hvac',
-        'ansible (>2.0.0)',
     ],
 )
