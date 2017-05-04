9e9108f177bc8bc9e2b1ebd240512cdf265a6c9f
Terry Howe <terrylhowe@gmail.com>
2017-05-04 14:26:48 -0600

    Cut new release for vault-token

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description=open('README.rst', 'r').read()
 
 setup(
     name='ansible-modules-hashivault',
-    version='2.10.0',
+    version='2.11.0',
     description='Ansible Modules for Hashicorp Vault',
     long_description=long_description,
     author='Terry Howe',
