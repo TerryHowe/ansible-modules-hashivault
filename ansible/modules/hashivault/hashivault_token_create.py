d011e844677e8052357a6ec939cbf68b2875ab7c
T.J. Telan <tjtelan@level11.com>
2017-06-08 16:56:47 -0700

    Supporting all of the options for the token create api call

--- ansible/modules/hashivault/hashivault_token_create.py
+++ ansible/modules/hashivault/hashivault_token_create.py
@@ -59,15 +59,20 @@ EXAMPLES = '''
 
 def main():
     argspec = hashivault_argspec()
-    argspec['lease'] = dict(required=False, type='str', default='1h')
-    argspec['role'] = dict(required=False, type='str', default='1h')
-    argspec['renewable'] = dict(required=False, type='bool', default=False)
-    argspec['orphan'] = dict(required=False, type='bool', default=False)
-    argspec['ttl'] = dict(required=False, type='str', default='1h')
-    argspec['policies'] = dict(required=True, type='str')
-    argspec['display_name'] = dict(required=True, type='str')
+    argspec['role'] = dict(required=False, type='str')
+    argspec['id'] = dict(required=False, type='str')
+    argspec['policies'] = dict(required=True, type='list')
     argspec['metadata'] = dict(required=False, type='str')
-    argspec['token'] = dict(required=False, type='str')
+    argspec['no_parent'] = dict(required=False, type='str')
+    argspec['lease'] = dict(required=False, type='str')
+    argspec['display_name'] = dict(required=True, type='str')
+    argspec['num_uses'] = dict(required=False, type='str')
+    argspec['no_default_policy'] = dict(required=False, type='bool', default=False)
+    argspec['ttl'] = dict(required=False, type='str')
+    argspec['wrap_ttl'] = dict(required=False, type='str')
+    argspec['orphan'] = dict(required=False, type='bool', default=False)
+    argspec['renewable'] = dict(required=False, type='bool')
+    argspec['explicit_max_ttl'] = dict(required=False, type='str')
     module = hashivault_init(argspec)
     result = hashivault_token_create(module.params)
     if result.get('failed'):
@@ -83,8 +88,38 @@ from ansible.module_utils.hashivault import *
 @hashiwrapper
 def hashivault_token_create(params):
     client = hashivault_auth_client(params)
+    role = params.get('role')
+    id = params.get('id')
     policies = params.get('policies')
-    token = client.create_token(policies=policies)
+    metadata = params.get('metadata')
+    no_parent = params.get('no_parent')
+    lease = params.get('lease')
+    display_name = params.get('display_name')
+    num_uses = params.get('num_uses')
+    no_default_policy = params.get('no_default_policy')
+    ttl = params.get('ttl')
+    wrap_ttl = params.get('wrap_ttl')
+    orphan = params.get('orphan')
+    renewable = params.get('renewable')
+    explicit_max_ttl = params.get('explicit_max_ttl')
+
+    token = client.create_token(
+        role=role,
+        id=id,
+        policies=policies,
+        meta=metadata,
+        no_parent=no_parent,
+        lease=lease,
+        display_name=display_name,
+        num_uses=num_uses,
+        no_default_policy=no_default_policy,
+        ttl=ttl,
+        wrap_ttl=wrap_ttl,
+        orphan=orphan,
+        renewable=renewable,
+        explicit_max_ttl=explicit_max_ttl
+        )
+
     return {'changed': True, 'token': token}
 
 if __name__ == '__main__':
