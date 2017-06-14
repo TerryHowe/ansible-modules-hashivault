810a1daf4d4371b8a45346e52d7acc0743cea333
Terry Howe <terrylhowe@gmail.com>
2017-06-14 09:24:36 -0600

    New release to set keys and threshold on init

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description=open('README.rst', 'r').read()
 
 setup(
     name='ansible-modules-hashivault',
-    version='3.0.0',
+    version='3.1.0',
     description='Ansible Modules for Hashicorp Vault',
     long_description=long_description,
     author='Terry Howe',
