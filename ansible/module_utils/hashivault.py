2dc90448e3cd7ea335d98ffe4d30d6de6b668b80
GIBSON, NICHOLAS R <ng560p@att.com>
2017-08-16 17:04:53 -0700

    removed print statement

--- ansible/module_utils/hashivault.py
+++ ansible/module_utils/hashivault.py
@@ -48,7 +48,6 @@ def hashivault_auth(client, params):
     elif authtype == 'approle':
         resp = client.auth_approle(role_id,secret_id)
         client.token= str(resp['auth']['client_token'])
-        print(client.token)
     else:
         client.token = token
     return client
