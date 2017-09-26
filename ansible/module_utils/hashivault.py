d423f4e6cc6353177f1cc0ffcd680c1554c0c739
Nathan Randall <nathan.randall@cloudvisory.com>
2017-09-25 19:46:51 -0600

    Revert "Revert "Add support for TLS connections via hvac client""
    This reverts commit 8866de9d3481fc5ca5188a5286d78f1f75b37b7c.

--- ansible/module_utils/hashivault.py
+++ ansible/module_utils/hashivault.py
@@ -9,6 +9,10 @@ from ansible.module_utils.basic import AnsibleModule
 def hashivault_argspec():
     argument_spec = dict(
         url = dict(required=False, default=os.environ.get('VAULT_ADDR', ''), type='str'),
+        ca_cert = dict(required=False, default=os.environ.get('VAULT_CACERT', ''), type='str'),
+        ca_path = dict(required=False, default=os.environ.get('VAULT_CAPATH', ''), type='str'),
+        client_cert = dict(required=False, default=os.environ.get('VAULT_CLIENT_CERT', ''), type='str'),
+        client_key = dict(required=False, default=os.environ.get('VAULT_CLIENT_KEY', ''), type='str'),
         verify = dict(required=False, default=(not os.environ.get('VAULT_SKIP_VERIFY', '')), type='bool'),
         authtype = dict(required=False, default='token', type='str'),
         token = dict(required=False, default=hashivault_default_token(), type='str', no_log=True),
@@ -24,15 +28,28 @@ def hashivault_init(argument_spec):
 
 def hashivault_client(params):
     url = params.get('url')
-    verify = params.get('verify')
-    client = hvac.Client(url=url, verify=verify)
+    ca_cert = params.get('ca_cert')
+    ca_path = params.get('ca_path')
+    client_cert = params.get('client_cert')
+    client_key = params.get('client_key')
+    cert = (client_cert, client_key)
+    check_verify = params.get('verify')
+    if check_verify == '' or check_verify;
+        if ca_cert:
+            verify = ca_cert
+        elif ca_path:
+            verify = ca_path
+        else
+            verify = check_verify
+    else:
+        verify = check_verify
+    client = hvac.Client(url=url, cert=cert, verify=verify)
     return client
 
 
 def hashivault_auth(client, params):
     token = params.get('token')
     authtype = params.get('authtype')
-    token = params.get('token')
     username = params.get('username')
     password = params.get('password')
     if authtype == 'github':
