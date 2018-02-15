9a1f877c942c71a92dd6f676c464a18503efe57e
Terry Howe <terrylhowe@gmail.com>
2018-02-15 13:41:48 -0700

    Add py3 tests

--- build.sh
+++ build.sh
@@ -1,17 +1,27 @@
 #!/bin/bash -ex
+function runtests() {
+    rm -rf dist
+    python setup.py sdist
+    if [ "$(uname)" == "Dawrin" ]; then
+        #brew install gmp
+        env "CFLAGS=-I/usr/local/include -L/usr/local/lib" pip install ./dist/ansible-modules-hashivault-*.tar.gz
+    else
+        pip install ./dist/ansible-modules-hashivault-*.tar.gz
+    fi
+    cd functional
+    ./run.sh
+    cd ..
+}
+
 rm -rf .testenv
 virtualenv .testenv
 source .testenv/bin/activate
-rm -rf dist
-python setup.py sdist
-
-if [ "$(uname)" == "Dawrin" ]; then
-  #brew install gmp
-  env "CFLAGS=-I/usr/local/include -L/usr/local/lib" pip install ./dist/ansible-modules-hashivault-*.tar.gz
-else
-  pip install ./dist/ansible-modules-hashivault-*.tar.gz
-fi
-
-cd functional
-./run.sh
+runtests
 rm -rf .testenv
+
+# Python 3
+rm -rf .testpy3
+virtualenv -p python3 .testpy3
+source .testpy3/bin/activate
+runtests
+rm -rf .testpy3
