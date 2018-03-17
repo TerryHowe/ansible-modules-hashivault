3c8e8961fb29d2d3cfa511ba136cbdb640834bbf
Terry Howe <terrylhowe@gmail.com>
2018-03-17 10:46:32 -0600

    Have upload push master

--- upload.sh
+++ upload.sh
@@ -5,15 +5,18 @@ pip install gitchangelog
 OLDVERSION=$(grep version setup.py | sed -e "s/.*='//" -e "s/',//")
 vi setup.py
 VERSION=$(grep version setup.py | sed -e "s/.*='//" -e "s/',//")
-if [ $OLDVERSION == $VERSION ]
+if [ "$OLDVERSION" == "$VERSION" ]
 then
-    echo 'Old version is the same'
+    echo 'Old version $OLDVERSION same as $VERSION'
     read ANS
 fi
 git tag -a $VERSION -m v$VERSION
 gitchangelog
 vi CHANGELOG.rst
+echo 'Ready to push $VERSION?'
+read ANS
 git commit -m "Version $VERSION" CHANGELOG.rst setup.py
+git push origin master
 git tag --force  -a $VERSION -m v$VERSION
 git push origin --tags
 #python setup.py register -r pypi
