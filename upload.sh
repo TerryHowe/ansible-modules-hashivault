a77796f88207af2e8a3974c55821dd236cd28913
Terry Howe <terrylhowe@gmail.com>
2018-03-17 10:02:06 -0600

    Add change log and gitchangelog

--- upload.sh
+++ upload.sh
@@ -1,8 +1,20 @@
 #
 # Make sure you have a ~/.pypirc file first
 #
+pip install gitchangelog
+OLDVERSION=$(grep version setup.py | sed -e "s/.*='//" -e "s/',//")
+vi setup.py
 VERSION=$(grep version setup.py | sed -e "s/.*='//" -e "s/',//")
+if [ $OLDVERSION == $VERSION ]
+then
+    echo 'Old version is the same'
+    read ANS
+fi
 git tag -a $VERSION -m v$VERSION
+gitchangelog
+vi CHANGELOG.rst
+git commit -m "Version $VERSION" CHANGELOG.rst setup.py
+git tag --force  -a $VERSION -m v$VERSION
 git push origin --tags
 #python setup.py register -r pypi
 python setup.py sdist upload -r pypi
