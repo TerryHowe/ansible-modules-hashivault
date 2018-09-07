a8163793e923a4292d0555e255a0b105d54eead9
Terry Howe <terrylhowe@gmail.com>
2018-09-07 08:43:02 -0600

    Fix list auth backends test

--- ansible/modules/hashivault/hashivault_auth_list.py
+++ ansible/modules/hashivault/hashivault_auth_list.py
@@ -74,7 +74,9 @@ from ansible.module_utils.hashivault import *
 @hashiwrapper
 def hashivault_auth_list(params):
     client = hashivault_auth_client(params)
-    return {'changed': True, 'backends': client.list_auth_backends()}
+    result = client.list_auth_backends()
+    result = result.get('data', result)
+    return {'changed': True, 'backends': result}
 
 
 if __name__ == '__main__':
