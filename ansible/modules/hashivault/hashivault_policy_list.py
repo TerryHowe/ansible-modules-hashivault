4b17cddb24d0640eb2c282241c0212986178100b
Terry Howe <terrylhowe@gmail.com>
2018-10-07 12:46:52 -0600

    Fix tests again

--- ansible/modules/hashivault/hashivault_policy_list.py
+++ ansible/modules/hashivault/hashivault_policy_list.py
@@ -76,8 +76,9 @@ from ansible.module_utils.hashivault import *
 def hashivault_policy_list(params):
     client = hashivault_auth_client(params)
     current_policies = client.list_policies()
-    current_policies = current_policies.get('data', current_policies)
-    current_policies = current_policies.get('policies', current_policies)
+    if isinstance(current_policies, dict):
+        current_policies = current_policies.get('data', current_policies)
+        current_policies = current_policies.get('policies', current_policies)
     return {'policies': current_policies}
 
 
