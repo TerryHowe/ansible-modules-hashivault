31fcc2406ff61a976061bf0f8fc57a606606e151
Terry Howe <terrylhowe@gmail.com>
2019-02-04 20:56:44 -0700

    Update upload to ignore gh pages

--- upload.sh
+++ upload.sh
@@ -21,8 +21,3 @@ git tag --force  -a $VERSION -m v$VERSION
 git push origin --tags
 #python setup.py register -r pypi
 python setup.py sdist upload -r pypi
-
-git checkout gh-pages
-git rebase master
-git push origin gh-pages
-git checkout master
