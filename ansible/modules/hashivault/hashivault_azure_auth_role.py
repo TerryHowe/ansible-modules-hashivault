c383b996333a646eddf9c806c2831d17fa7047fc
Drew Mullen <drew@nebulaworks.com>
2019-05-29 08:16:22 -0400

    state allows present/absent behavior
    cleanup

--- ansible/modules/hashivault/hashivault_azure_auth_role.py
+++ ansible/modules/hashivault/hashivault_azure_auth_role.py
@@ -96,18 +96,21 @@ options:
             - The list of scale set names that the login is restricted to.
     role_file:
         description:
-            - file with a single dict, pass all variables except name in this instead of ansible yaml
+            - file with a json object containing play parameters. pass all params but name, state, mount_point which stay in the ansible play
 '''
 EXAMPLES = '''
 ---
 - hosts: localhost
   tasks:
-    - hashivault_azure_auth_role:
-        name: contributor-role
-        
+      hashivault_azure_auth_role:
+        name: "test"
+        policies: ["test"]
+        bound_subscription_ids: ["6a1d5988-5917-4221-b224-904cd7e24a25"]
+        num_uses: 3
 
     - hashivault_azure_auth_role:
-        name: contributor-role
+        name: test
+        role_file: /users/drewbuntu/my-auth-role.json
 '''
 
 
@@ -130,7 +133,7 @@ def main():
     argspec['num_uses'] = dict(required=False, type='int', default=0)
 
     supports_check_mode=True
-    required_one_of=[['bound_service_principal_ids', 'bound_group_ids', 'bound_locations', 'bound_subscription_ids', 'bound_resource_groups', 'bound_scale_sets', 'role_file']]
+    required_one_of=[['bound_service_principal_ids', 'bound_group_ids', 'bound_locations', 'bound_subscription_ids', 'bound_resource_groups', 'bound_scale_sets', 'role_file', 'state']]
 
     module = hashivault_init(argspec, supports_check_mode, required_one_of)
     result = hashivault_azure_auth_role(module)
@@ -149,10 +152,7 @@ def hashivault_azure_auth_role(module):
     name = params.get('name')
     state = params.get('state')
     desired_state = dict()
-
-    # grab from file
-
-
+    exists = False
     changed = False
 
     # do not want a trailing slash in name
@@ -161,13 +161,10 @@ def hashivault_azure_auth_role(module):
     if mount_point[-1]:
         mount_point = mount_point.strip('/')
 
-    # input validation required for policies var?
-
     # if azure_role_file is set, set azure_role to contents
     # else assume azure_role is set and use that value
     if role_file:
         desired_state = json.loads(open(params.get('role_file'), 'r').read())
-        print(desired_state)
     else:
         desired_state['policies'] = params.get('policies')
         desired_state['ttl'] = params.get('token_ttl')
@@ -189,50 +186,34 @@ def hashivault_azure_auth_role(module):
     # check if role exists
     try:
         existing_roles = client.auth.azure.list_roles(mount_point=mount_point)
-        if name not in existing_roles['keys']:
-            # some roles exist but not this one
-            changed = True
+        if name in existing_roles['keys']:
+            # this role exists
+            exists = True
     except:
         # no roles exist yet
+        pass
+
+    if not exists and state == 'present':
         changed = True
 
     # compare current_state to desired_state
-    if not changed:
+    if exists and state == 'present' and not changed:
         current_state = client.auth.azure.read_role(name=name)   
         for k, v in desired_state.items():
-            print("desired ", k, " ", v, " type: ", type(v))
-            print("current_state ", k, " ", current_state[k], " type: ", type(current_state[k]))
             if v != current_state[k]:
                 changed = True
-                print('FIRE ')
-        
-
+    elif exists and state == 'absent':
+        changed = True
+    
     # make the changes!
     # NOTE: bound_location is paseed. will need to be changed eventually
     # https://github.com/hvac/hvac/issues/451
-    if changed and not module.check_mode:
-        result = client.auth.azure.create_role(name=name, mount_point=mount_point, **desired_state)
-        # ttl=token_ttl, max_ttl=token_max_ttl, 
-        #     period=token_period, bound_service_principal_ids=bound_service_principal_ids, bound_group_ids=bound_group_ids, bound_location=bound_locations, 
-        #     bound_subscription_ids=bound_subscription_ids, bound_resource_groups=bound_resource_groups, bound_scale_sets=bound_scale_sets)
-
-    # # azure_role comes from json which is assigned as a str object type, convert to py objs
-    # azure_role = literal_eval(azure_role)
-    # if not changed:
-    #     # check if role content == desired
-    #     current = client.secrets.aws.read_role(name=name,mount_point=mount_point)['data']['azure_roles']
-    #     caught = 0
-    #     for i in azure_role:
-    #         for i2 in current:
-    #             if i.items() <= i2.items():
-    #                 caught = caught + 1
-    #     if caught != len(azure_role) or caught != len(current):
-    #         changed = True
-
-    # # make the changes!
-    # if changed and not module.check_mode:
-    #     client.secrets.azure.create_or_update_role(name=name, azure_roles=azure_role)
+    if changed and state == 'present' and not module.check_mode:
+        client.auth.azure.create_role(name=name, mount_point=mount_point, **desired_state)
     
+    elif changed and state == 'absent' and not module.check_mode:
+        client.auth.azure.delete_role(name=name, mount_point=mount_point)
+
     return {'changed': changed}
 
 
