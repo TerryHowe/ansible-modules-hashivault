15f6e5716ce2ea26c48d89477f5a97ed5ea9abd2
Lynn Dong <ldong@nebulaworks.com>
2019-10-15 07:59:40 -0700

    Enable OIDC auth config functional test

--- functional/run.sh
+++ functional/run.sh
@@ -23,7 +23,7 @@ ansible-playbook -v test_auth.yml
 ansible-playbook -v test_auth_method.yml
 ansible-playbook -v test_azure_auth_config.yml
 ansible-playbook -v test_azure_auth_role.yml
-# ansible-playbook -v test_oidc_auth_method_config.yml cannot run without enterprise
+ansible-playbook -v test_oidc_auth_method_config.yml
 ansible-playbook -v test_secret_list.yml
 # ansible-playbook -v test_namespace.yml cannot run without enterprise 
 # ansible-playbook -v test_db_config.yml cannot run without true db connectivity
