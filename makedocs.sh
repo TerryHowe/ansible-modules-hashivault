e26b4be7f46218a58004fc4ad4527feaec7664bc
Dr.MagPie <ShadowSega15@gmail.com>
2020-04-15 22:24:04 +0300

    Updated makedocs.sh to use doc_fragments

--- makedocs.sh
+++ makedocs.sh
@@ -2,13 +2,15 @@
 export PLUGINS=''
 rm -rf ansible-repo
 mkdir ansible-repo && cd ansible-repo
-git clone https://github.com/ansible/ansible.git && cd ansible &&  git checkout v2.7.6 && cd ..
+git clone --branch 'v2.9.6' https://github.com/ansible/ansible.git
 pip install sphinx sphinx_rtd_theme
 pip install -r ansible/requirements.txt
 rm -rf ansible/lib/ansible/modules/ && mkdir -p ansible/lib/ansible/modules/hashivault
 cp -r ../ansible/modules/hashivault/hashivault*.py ansible/lib/ansible/modules/hashivault/
+rm -f ansible/lib/ansible/plugins/doc_fragments/hashivault.py
+cp {..,ansible/lib}/ansible/plugins/doc_fragments/hashivault.py
 ls ansible/lib/ansible/modules/hashivault
-export MODULES=$(ls -m ansible/lib/ansible/modules/hashivault/ | grep -v '^_' | tr -d '[:space:]'  | sed 's/.py//g')
+export MODULES=$(ls -m ansible/lib/ansible/modules/hashivault/ | grep -v '^_' | tr -d '[:space:]' | sed 's/.py//g')
 cd ansible/docs/docsite/
 make webdocs || true
 touch _build/html/.nojekyll
