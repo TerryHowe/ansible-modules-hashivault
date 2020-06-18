9a43c897fe7f9605f3a14724dc0bc8e993a6b966
Gregor Riepl <Gregor.Riepl@swisstxt.ch>
2020-06-18 11:35:13 +0200

    Corrected module_utils path

--- plugins/modules/hashivault/hashivault_k8s_auth_config.py
+++ plugins/modules/hashivault/hashivault_k8s_auth_config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-from ansible.module_utils.hashivault import hashivault_argspec
-from ansible.module_utils.hashivault import hashivault_auth_client
-from ansible.module_utils.hashivault import hashivault_init
-from ansible.module_utils.hashivault import hashiwrapper
-from ansible.module_utils.hashivault import get_keys_updated
+from ansible_collections.terryhowe.hashivault.plugins.module_utils.hashivault import hashivault_argspec
+from ansible_collections.terryhowe.hashivault.plugins.module_utils.hashivault import hashivault_auth_client
+from ansible_collections.terryhowe.hashivault.plugins.module_utils.hashivault import hashivault_init
+from ansible_collections.terryhowe.hashivault.plugins.module_utils.hashivault import hashiwrapper
+from ansible_collections.terryhowe.hashivault.plugins.module_utils.hashivault import get_keys_updated
 from hvac.exceptions import InvalidPath
 
 ANSIBLE_METADATA = {'status': ['stableinterface'], 'supported_by': 'community', 'version': '1.1'}
