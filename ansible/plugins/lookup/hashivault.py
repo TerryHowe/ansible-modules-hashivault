7062a6e20ddb3943a0eeabdb178d7a37b660b900
Terry Howe <terrylhowe@gmail.com>
2020-10-31 10:36:25 -0600

    Have lookup plugin use argspec

--- ansible/plugins/lookup/hashivault.py
+++ ansible/plugins/lookup/hashivault.py
@@ -16,84 +16,50 @@ import os
 import sys
 
 from ansible.errors import AnsibleError
+from ansible.module_utils.basic import AnsibleFallbackNotFound
 from ansible.plugins.lookup import LookupBase
 
-from ansible.module_utils.hashivault import (
-    hashivault_default_token,
-    hashivault_read,
-)
+from ansible.module_utils.hashivault import hashivault_argspec
+from ansible.module_utils.hashivault import hashivault_read
 
 
 class LookupModule(LookupBase):
-    @staticmethod
-    def _get_environment(environments, name, default_value=None):
-        for env in environments:
-            if name in env:
-                return env.get(name)
-        return os.getenv(name, default_value)
 
-    def _get_url(self, environments):
-        url = self._get_environment(environments, 'VAULT_ADDR')
-        if url:
-            return url.rstrip('/')
-        return "https://127.0.0.1:8200"
+    def _get_params(self, argspec, terms, kwargs):
+        params = {}
+        for arg in argspec:
+            spec = argspec[arg]
+            if arg in kwargs:
+                params[arg] = kwargs[arg]
+            elif 'default' in spec:
+                params[arg] = spec.get('default')
+            elif 'fallback' in spec:
+                fall_func, fall_args = spec.get('fallback')
+                try:
+                    params[arg] = fall_func(*tuple(fall_args))
+                except AnsibleFallbackNotFound:
+                    pass
 
-    def _get_params(self, terms, environments, kwargs):
-        path = terms[0]
+        params['secret'] = terms[0]
         try:
-            key = terms[1]
+            params['key'] = terms[1]
         except IndexError:
-            key = None
-        default = kwargs.get('default', None)
-        version = kwargs.get('version')
-        mount_point = kwargs.get('mount_point', 'secret')
-        params = {
-            'url': self._get_url(environments),
-            'verify': self._get_verify(environments),
-            'secret': path,
-            'key': key,
-            'default': default,
-            'version': version,
-            'mount_point': mount_point,
-        }
-        authtype = self._get_environment(environments, 'VAULT_AUTHTYPE', 'token')
-        params['authtype'] = authtype
-        login_mount_point = self._get_environment(environments, 'VAULT_LOGIN_MOUNT_POINT', authtype)
-        params['login_mount_point'] = login_mount_point
-        cacert = self._get_environment(environments, 'VAULT_CACERT')
-        if cacert:
-            params['ca_cert'] = cacert
-        capath = self._get_environment(environments, 'VAULT_CAPATH')
-        if capath:
-            params['ca_path'] = capath
-        params['client_cert'] = os.getenv('VAULT_CLIENT_CERT')
-        params['client_key'] = os.getenv('VAULT_CLIENT_KEY')
-        if authtype == 'approle':
-            params['role_id'] = self._get_environment(environments, 'VAULT_ROLE_ID')
-            params['secret_id'] = self._get_environment(environments, 'VAULT_SECRET_ID')
-        elif authtype == 'userpass' or authtype == 'ldap':
-            params['username'] = self._get_environment(environments, 'VAULT_USER')
-            params['password'] = self._get_environment(environments, 'VAULT_PASSWORD')
-        elif authtype == 'aws':
-            params['aws_header'] = self._get_environment(environments, 'VAULT_AWS_HEADER')
-        else:
-            params['token'] = self._get_environment(environments, 'VAULT_TOKEN', hashivault_default_token())
+            params['key'] = None
         return params
 
-    def _get_verify(self, environments):
-        capath = self._get_environment(environments, 'VAULT_CAPATH')
-        if capath:
-            return capath
-        cacert = self._get_environment(environments, 'VAULT_CACERT')
-        if cacert:
-            return cacert
-        if self._get_environment(environments, 'VAULT_SKIP_VERIFY'):
-            return False
-        return True
-
     def run(self, terms, variables=None, **kwargs):
-        environments = variables.get('environment', [])
-        result = hashivault_read(self._get_params(terms, environments, kwargs))
+        # self._display.v('Running lookup')
+        argspec = hashivault_argspec()
+        argspec['version'] = dict(required=False, type='int', default=1)
+        argspec['mount_point'] = dict(required=False, type='str', default='secret')
+        argspec['secret'] = dict(required=True, type='str')
+        argspec['key'] = dict(required=False, type='str')
+        argspec['default'] = dict(required=False, default=None, type='str')
+        params = self._get_params(argspec, terms, kwargs)
+        # self._display.v('ARGSPEC: ' + str(argspec))
+        # self._display.v('KWARGS: ' + str(kwargs))
+        # self._display.v('PARAMS: ' + str(params))
+        result = hashivault_read(params=params)
         if 'value' not in result:
             path = terms[0]
             try:
