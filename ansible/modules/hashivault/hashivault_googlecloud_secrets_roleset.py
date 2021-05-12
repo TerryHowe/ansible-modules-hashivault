ea4193dcd2710bafe8885c7fd53104abf3ad8745
Mickael Dangleterre <michael.dangleterre@gmail.com>
2021-05-12 13:30:14 +0200

    Update for hashivault_googlecloud_secrets_roleset task New task : hashivault_googlecloud_secrets_roleset

--- ansible/modules/hashivault/hashivault_googlecloud_secrets_roleset.py
+++ ansible/modules/hashivault/hashivault_googlecloud_secrets_roleset.py
@@ -3,21 +3,21 @@ from ansible.module_utils.hashivault import hashivault_argspec
 from ansible.module_utils.hashivault import hashivault_auth_client
 from ansible.module_utils.hashivault import hashivault_init
 from ansible.module_utils.hashivault import hashiwrapper
-import json
 
 
 def main():
     argspec = hashivault_argspec()
     argspec['name'] = dict(required=True, type='str')
-    argspec['secret_type'] = dict(required=False, type='str', choices=['access_token', 'service_account'],
-                                  default='access_token')
+    argspec['state'] = dict(required=False, type='str', default='present', choices=['present', 'absent'])
     argspec['project'] = dict(required=True, type='str')
+    argspec['secret_type'] = dict(required=True, type='str', default=None,
+                                  choices=['access_token', 'service_account_key'])
     argspec['bindings'] = dict(required=True, type='str')
     argspec['token_scopes'] = dict(required=False, type='list', default=[])
-    argspec['state'] = dict(required=False, type='str', choices=['present', 'update', 'absent'], default='present')
-    argspec['mount_point'] = dict(required=True, type='str')
+    argspec['mount_point'] = dict(required=False, type='str', default='gcp')
     module = hashivault_init(argspec, supports_check_mode=True)
     result = hashivault_googlecloud_secrets_roleset(module)
+
     if result.get('failed'):
         module.fail_json(**result)
     else:
@@ -27,33 +27,36 @@ def main():
 @hashiwrapper
 def hashivault_googlecloud_secrets_roleset(module):
     params = module.params
-    mount_point = params.get('mount_point')
+    client = hashivault_auth_client(params)
+    state = params.get('state')
+    mount_point = params.get('mount_point').strip('/')
+    project = params.get('project')
+    bindings = params.get('bindings')
+    secret_type = params.get('secret_type')
+    name = params.get('name')
+    token_scopes = params.get('token_scopes')
     desired_state = dict()
     current_state = dict()
     changed = False
-    client = hashivault_auth_client(params)
-    state = params.get('state')
-    name = params.get('name')
-    secret_type = params.get('secret_type')
-    project = params.get('project')
-
-    desired_state['name'] = name
-    desired_state['secret_type'] = secret_type
-    desired_state['project'] = project
-    desired_state['bindings'] = params.get('bindings')
 
     if secret_type == 'access_token':
-        desired_state['token_scopes'] = params.get('token_scopes')
+        desired_state['token_scopes'] = token_scopes
+    elif secret_type == 'service_account_key':
+        desired_state['bindings'] = bindings
+    elif secret_type is None:
+        desired_state['bindings'] = bindings
+        desired_state['token_scopes'] = token_scopes
 
     try:
-        current_state = client.secrets.read_roleset()
+        current_state = client.secrets.gcp.read_roleset(name=name)
     except Exception:
         changed = True
 
-    if state == 'present' or state == 'update':
-        client.secrets.create_or_update_roleset(mount_point=mount_point, **desired_state)
-    else:
-        client.secrets.delete_roleset(mount_point=mount_point, name=name)
+    if changed and not module.check_mode and state == 'present':
+        client.secrets.gcp.create_or_update_roleset(mount_point=mount_point, name=name, project=project
+                                                    , **desired_state)
+
+    return {'changed': True}
 
 
 if __name__ == '__main__':
