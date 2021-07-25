71e5a2a0e249ce578d4c01d844ca155275d706d1
Terry Howe <terrylhowe@gmail.com>
2021-07-25 06:12:52 -0600

    Fix list tests

--- ansible/modules/hashivault/hashivault_list.py
+++ ansible/modules/hashivault/hashivault_list.py
@@ -95,7 +95,7 @@ def hashivault_list(params):
 
     try:
         if version == 2:
-            if 'metadata' in secret:
+            if secret and metadata:
                 response = client.secrets.kv.v2.read_secret_metadata(path=secret, mount_point=mount_point)
                 result['metadata'] = response.get('data', {})
             else:
