af5731c3ebd748f93186dede556e9fc05805a722
Terry Howe <terrylhowe@gmail.com>
2022-11-07 13:31:59 -0700

    Version 4.7.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = open('README.rst', 'r').read()
 
 setup(
     name='ansible-modules-hashivault',
-    version='4.7.0',
+    version='4.7.1',
     description='Ansible Modules for Hashicorp Vault',
     long_description=long_description,
     long_description_content_type='text/x-rst',
